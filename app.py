5b7ed7dde44fd58388d27917623aa6b8ecd94c28
PathakSiddhant <siddhantpathak1207@gmail.com>
2025-12-18 11:53:08 +0530

    Created Virtual Environment, installed libraries and tools

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Legal Risk Analyzer", layout="wide")
+
+st.title("⚖️ Legal Risk Analyzer AI")
+st.write("Bhai, setup successful hai! Humara UI ready ho raha hai.")
+
+st.button("Click Me")
